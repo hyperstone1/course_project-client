989ea14f6a34a1dd6441cfbf56f6d5430036932a
hyperstone1 <kingofthegorgon@mail.ru>
2022-12-11 15:50:01 +0300

    fixed warnings

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import './scss/index.scss';
 import Login from './pages/Login/Login';
 import Registration from './pages/Registration/Registration';
-import { Routes, Route, useNavigate, useSearchParams, useParams } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import qs from 'qs';
 import { useDispatch, useSelector } from 'react-redux';
 import { setLogin } from './store/slices/auth/authSlice';
 
 function App() {
   const isLogin = useSelector((state) => state.auth.isLogin);
   const dispatch = useDispatch();
-  const isMounted = useRef(null);
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const params = useParams();
 
   useEffect(() => {
     console.log(window.location.pathname);
@@ -23,13 +18,7 @@ function App() {
     } else {
       dispatch(setLogin({ isLogin: false }));
     }
-  }, [isLogin]);
-
-  useEffect(() => {
-    if (params) {
-      console.log(params);
-    }
-  }, []);
+  }, [isLogin, dispatch]);
 
   return (
     <div className="App">
