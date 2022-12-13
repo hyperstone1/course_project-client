e6cf70362d58140f38d158e0371923ebe8f6508c
hyperstone1 <kingofthegorgon@mail.ru>
2022-12-13 16:34:46 +0300

    save menu value in redux

--- src/components/ProfileMenu/ProfileMenu.jsx
+++ src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Nav from 'react-bootstrap/Nav';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMenuValue } from '../../store/slices/profileSlice/profileSlice';
 
 const ProfileMenu = () => {
+  const dispatch = useDispatch();
+  const menuKey = useSelector((state) => state.profile.menuValue);
+
+  useEffect(() => {
+    console.log(menuKey);
+  }, [menuKey]);
   return (
     <div className="nav_profile">
-      <Nav justify variant="tabs" style={{ flexDirection: 'column' }} defaultActiveKey="link-1">
+      <Nav
+        justify
+        variant="tabs"
+        style={{ flexDirection: 'column' }}
+        onSelect={(key) => dispatch(setMenuValue({ menuValue: key }))}
+        defaultActiveKey="info"
+      >
         <Nav.Item>
-          <Nav.Link eventKey="link-1">Personal Information</Nav.Link>
+          <Nav.Link eventKey="info">Personal Information</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-2">My reviews</Nav.Link>
+          <Nav.Link eventKey="reviews">My reviews</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-3">My comments</Nav.Link>
+          <Nav.Link eventKey="comments">My comments</Nav.Link>
         </Nav.Item>
       </Nav>
     </div>
--- src/store/slices/profileSlice/profileSlice.js
+++ src/store/slices/profileSlice/profileSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  menuValue: 'personal information',
+  menuValue: 'info',
 };
 
 const profileSlice = createSlice({
