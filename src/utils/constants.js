325339d42bf0a2f72bcca57e5987c71641cc226d
Nikolay Parfenchik <43679849+hyperstone1@users.noreply.github.com>
2023-01-10 16:30:56 +0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-export const host = 'http://localhost:5000';
-=======
 export const host = 'https://jovial-kelpie-121aa5.netlify.app';
->>>>>>> 658bbec12a99fdae8121f2e241b13511bbc67bab
+
